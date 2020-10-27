f1ec243886c14795eb1bcf5b7f56fd9f42f46792
ValdemarBirk <71699913+ValdemarBirk@users.noreply.github.com>
2020-10-27 11:56:02 +0100

    Navngivning + inventory

--- zuul-framework/worldofzuul/Main.java
+++ zuul-framework/worldofzuul/Main.java
@@ -2,7 +2,9 @@ package worldofzuul;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.play();
+       // Game game = new Game();
+      //  game.play();
+        Player.newPlayerObject();
+
     }
 }
---
+++ zuul-framework/worldofzuul/Player.java
@@ -0,0 +1,41 @@
+package worldofzuul;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Player {
+    private String name;
+    private ArrayList<Plastic> plasticInv = new ArrayList();
+    // private ArrayList<Tool> toolInv = new ArrayList();
+    // Her er der taget udgangspunkt i et "Tool" objekt og et "Plastic" objekt.
+
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public static Player newPlayerObject() {
+        System.out.print("What is your name?> ");
+        boolean nameChosen = false;
+        String name = "";
+        while (!nameChosen) { // In this while loop we check for a name that is valid (No only space names) etc...
+            Scanner playerName = new Scanner(System.in);
+            name = playerName.nextLine();
+            if (name.matches(".*[0-9].*") || name.matches(".*[A-Z]*.")) {
+                nameChosen = true;
+            } else {
+                System.out.print("Name not vaild enter new name> ");
+            }
+
+        }
+        System.out.println("You have chosen " + name + " as your player name");
+        return new Player(name);
+    }
+
+    public void plasticCollect(Plastic piece) {
+        this.plasticInv.add(piece);
+
+    }
+
+}
+
