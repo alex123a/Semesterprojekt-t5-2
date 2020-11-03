ffd30573db0858bb609430aaaa6f3967e285f3e2
ValdemarBirk <71699913+ValdemarBirk@users.noreply.github.com>
2020-11-03 13:52:54 +0100

    lol

--- zuul-framework/worldofzuul/RoadBuilder.java
+++ zuul-framework/worldofzuul/RoadBuilder.java
@@ -13,7 +13,7 @@ public class RoadBuilder {
         if (inventory.size() < Game.getRoadDone()) {
             int inv = ((inventory.size()*100)/Game.getRoadDone());
             System.out.println("The road is " + inv + "% Complete");
-            System.out.println("You need to collect " + (Game.getRoadDone()-inventory.size()));
+            System.out.println("You need to collect " + (Game.getRoadDone()-inventory.size()) + " to finish the road ");
         }
         return inventory;
     }
