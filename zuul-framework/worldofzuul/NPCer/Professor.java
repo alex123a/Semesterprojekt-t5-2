35fb402e2dd27d91e65eaf0b3a8744c748ed44cb
Frederik Nielsen <frederik@icsharp.io>
2020-11-03 10:19:18 +0100

    Implementet professor npc

--- zuul-framework/worldofzuul/NPCer/Professor.java
+++ zuul-framework/worldofzuul/NPCer/Professor.java
@@ -1,15 +1,49 @@
 package worldofzuul.NPCer;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Professor extends NPC {
+    //Insert path into config file.
+    private String file = Paths.get(new File("worldofzuul/NPC/NPC-descriptions/ProfessorText.txt").getAbsolutePath()).toString();
+
     public Professor(String name) {
         super(name);
     }
 
     @Override
-    public File description() {
-        return null;
+    public void description(String input) {
+        boolean talking=false;
+        if (input.equals("talk")){
+            try {
+                String line;
+                line = Files.readAllLines(Paths.get(this.file)).get(1);
+                System.out.println(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Could not find text file");
+            }
+        } else if (input.equals("bye") && talking){
+            try {
+                String line;
+                line = Files.readAllLines(Paths.get(this.file)).get(2);
+                System.out.println(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Could not find text file");
+            }
+        } else {
+            try {
+                String line;
+                line = Files.readAllLines(Paths.get(this.file)).get(3);
+                System.out.println(line);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Could not find text file");
+            }
+        }
     }
 
     @Override
