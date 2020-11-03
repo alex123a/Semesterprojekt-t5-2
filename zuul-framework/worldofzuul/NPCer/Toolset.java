e0d2c8ad5a1986628aabeda12e8671de59b9f81f
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2020-11-03 14:23:01 +0100

    Player interaction with NPC

--- zuul-framework/worldofzuul/NPCer/Toolset.java
+++ zuul-framework/worldofzuul/NPCer/Toolset.java
@@ -9,11 +9,15 @@ public class Toolset {
         boolean repaired = false;
         // Mangler at tilføje tid, således statementet under bliver kørt en gang i sekundet.
 
-        while(RoadBuilder.getDamaged() <= 0) {
+        while(RoadBuilder.getDamaged() != 0 || RoadBuilder.getDamaged() > 0) {
             long millis = System.currentTimeMillis();
             RoadBuilder.setDamaged(RoadBuilder.getDamaged() - repair);
             System.out.println(100-RoadBuilder.getDamaged() + "% repaired");
-            Thread.sleep(1000 - millis % 1000);
+            try {
+                Thread.sleep(1000 - millis % 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
         return repaired = true;
