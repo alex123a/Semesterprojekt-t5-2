d0f94e1badb1075ff58e24790df13706d071c647
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2020-11-17 12:49:59 +0100

    sejt

--- GUIbaseret/src/sample/presentation/Controller.java
+++ GUIbaseret/src/sample/presentation/Controller.java
@@ -1,4 +1,15 @@
-package sample;
+package sample.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class Controller {
+
+    @FXML
+    private ImageView backgroundRoom;
+
+    @FXML
+    private Button changePic;
+
 }
