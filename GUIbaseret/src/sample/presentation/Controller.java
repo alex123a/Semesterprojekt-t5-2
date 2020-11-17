51ad0f5e63823882e14f6b6dee9a6f43411b5d19
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2020-11-17 13:09:29 +0100

    A nice beginning

--- GUIbaseret/src/sample/presentation/Controller.java
+++ GUIbaseret/src/sample/presentation/Controller.java
@@ -1,15 +1,24 @@
 package sample.presentation;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Controller {
+
+    public static String background = "file:src/sample/presentation/pictures/RoadBuild.png";
 
     @FXML
     private ImageView backgroundRoom;
 
     @FXML
     private Button changePic;
+
+    public void changeBackground(ActionEvent event) {
+        backgroundRoom.setImage(new Image(background));
+        System.out.println("Hej");
+    }
 
 }
