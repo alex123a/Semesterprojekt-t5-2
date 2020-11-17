0c20974f9548141e21f03954059c7ef9e63fd73b
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2020-11-17 13:55:49 +0100

    First two rooms implemented

--- GUIbaseret/src/sample/domain/Game.java
+++ GUIbaseret/src/sample/domain/Game.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 import sample.presentation.Controller;
 
 public class Game {
-    private Parser parser;
     private Room currentRoom;
     final private File welcomeMessage = new File("src/sample/data/textfiles/gameDescriptions/gameDescription.txt");
     final private File help = new File("src/sample/data/textfiles/gameDescriptions/help.txt");
@@ -25,7 +24,6 @@ public class Game {
 
     public Game() {
         createRooms();
-        parser = new Parser();
     }
 
 
@@ -60,11 +58,11 @@ public class Game {
         printWelcome();
 
         boolean finished = false;
+        /*
         while (!finished) {
             RoadBuilder.damagedMachine();
-            Command command = parser.getCommand();
-            finished = processCommand(command);
         }
+        */
     }
 
     private void printWelcome() {
@@ -84,85 +82,7 @@ public class Game {
         Timer.setStartTime();
     }
 
-    private boolean processCommand(Command command) {
-        boolean wantToQuit = false;
 
-        CommandWord commandWord = command.getCommandWord();
-
-        if (commandWord == CommandWord.UNKNOWN) {
-            System.out.println("I don't know what you mean...");
-            return false;
-        }
-
-        if (commandWord == CommandWord.HELP) {
-            printHelp();
-        } else if (commandWord == CommandWord.GO) {
-            goRoom(command);
-        } else if (commandWord == CommandWord.QUIT) {
-            wantToQuit = quit(command);
-        } else if (commandWord == CommandWord.TALK) {
-            if (currentRoom == Farm) {
-                farmer.description("talk");
-            } else if (currentRoom == Town) {
-                mechanic.description("talk");
-            } else if (currentRoom == Sdu) {
-                professor.description("talk");
-            }
-        } else if (commandWord == CommandWord.INFORMATION) {
-            if (currentRoom == Farm) {
-                farmer.description("information");
-            } else if (currentRoom == Town) {
-                mechanic.description("information");
-            }
-        } else if (commandWord == CommandWord.TAKE) {
-            if (currentRoom == Farm) {
-                farmer.description("take");
-            } else if (currentRoom == Town) {
-                mechanic.description("take");
-            }
-        } else if (commandWord == CommandWord.BYE) {
-            if (currentRoom == Farm) {
-                farmer.description("bye");
-            } else if (currentRoom == Sdu) {
-                professor.description("bye");
-            } else if (currentRoom == Town) {
-                mechanic.description("bye");
-            }
-        } else if (commandWord == commandWord.COLLECT) {
-            if (Player.getPlasticInv().size() < 10) {
-                Player.plasticCollect(currentRoom.getPlastic(), currentRoom);
-            } else {
-                System.out.println("Your inventory is full");
-            }
-        } else if (commandWord == commandWord.REPAIR && currentRoom == RoadBuild) {
-            if (Player.getHaveToolset()) {
-                try {
-                    toolset.repairMachine(RoadBuilder.getDamaged());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else if (commandWord == CommandWord.GIVE) {
-            if (RoadBuilder.getDamaged() == 0) {
-                if (currentRoom == RoadBuild) {
-                    if (givePlastic(command)) {
-                        System.out.println("You have completed 100% of the road in plastic.");
-                        Timer.setEndTime();
-                        Timer.timeScore();
-                        Timer.setHighScore();
-                        wantToQuit = true;
-                    }
-                } else {
-                    System.out.println("Go to the Roadbuilder to give plastic");
-                }
-            } else {
-                System.out.println("Machine \"i am broken, can't help you\"");
-            }
-        } else if (commandWord == commandWord.COLLECT) {
-            Player.plasticCollect(currentRoom.getPlastic(), currentRoom);
-        }
-        return wantToQuit;
-    }
 
     private void printHelp() {
 
@@ -178,18 +98,13 @@ public class Game {
             System.out.println("Cannot find the file");
             e.printStackTrace();
         }
-        parser.showCommands();
     }
 
-    private void goRoom(Command command) {
-        if (!command.hasSecondWord()) {
-            System.out.println("Go where?");
-            return;
-        }
+    // private void goRoom() {
 
-        String direction = command.getSecondWord();
-
-        Room nextRoom = currentRoom.getExit(direction);
+    public void goRoom() {
+        // Room nextRoom = currentRoom.getExit();
+        Room nextRoom = Park;
 
         if (nextRoom == null) {
             System.out.println("That is not possible!");
@@ -200,11 +115,7 @@ public class Game {
         }
     }
 
-    private boolean givePlastic(Command command) {
-        if (command.hasSecondWord()) {
-            System.out.println("Do you want to give plastic? Just write give");
-            return false;
-        }
+    private boolean givePlastic() {
         List<Plastic> plasticInv = Player.getPlasticInv();
         List<Plastic> road = RoadBuilder.inventory(plasticInv);
         Player.resetPlasticInv();
@@ -212,16 +123,6 @@ public class Game {
             return true;
         }
         return false;
-    }
-
-    private boolean quit(Command command) {
-        if (command.hasSecondWord()) {
-            System.out.println("Quit");
-            return false;
-        } else {
-            return true;
-        }
-
     }
 
     public static int getRoadDone() {
--- GUIbaseret/src/sample/presentation/Main.java
+++ GUIbaseret/src/sample/presentation/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.play();
+        game.goRoom();
         launch(args);
     }
 }
