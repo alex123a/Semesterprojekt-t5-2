d6b2fa8da78d4e3a5663dab5bd8976f2a8e97686
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2020-11-18 12:19:20 +0100

    Pane change

--- GUIbaseret/src/sample/presentation/Main.java
+++ GUIbaseret/src/sample/presentation/Main.java
@@ -17,7 +17,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         Game game = new Game();
         game.goRoom();
