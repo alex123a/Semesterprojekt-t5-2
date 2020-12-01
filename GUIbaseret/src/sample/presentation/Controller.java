c1814980907b269dda2f672a30e37450da9ea61d
JanikSunke <janik.sunke@hotmail.dk>
2020-12-01 14:26:04 +0100

    Revert "Revert "boolean""
    This reverts commit a47d756f3981a7d5d26b1940284cc202d8ac2211.

--- GUIbaseret/src/sample/presentation/Controller.java
+++ GUIbaseret/src/sample/presentation/Controller.java
@@ -31,6 +31,7 @@ public class Controller {
     private long animationWalk = 0;
     private long animationRoadbuilder = 0;
     private boolean gameNotStarted = true;
+    private boolean stopAnimation = false;
     private ObservableList<ImageView> inventoryObservable = FXCollections.observableList(new ArrayList<ImageView>());
 
     @FXML
@@ -444,7 +445,7 @@ public class Controller {
         if (Main.game.getCurrentRoom() instanceof RoadBuild) {
             roadbuilderTimer.start();
             System.out.println("test 1");
-            if (animationRoadbuilder == playerObject.getPlasticInv().size()*10) {
+            if (stopAnimation) {
                 roadbuilderTimer.stop();
                 System.out.println("afslut");
             }
@@ -459,16 +460,19 @@ public class Controller {
         @Override
         public void handle(long l) {
             if (roadBuilder.getInventoryCount() < 30 && animationRoadbuilder < playerObject.getPlasticInv().size()*10) {
-                if (animationRoadbuilder % 13 == 0) {
-                    System.out.println("handler");
-                    roadView.setViewport(new Rectangle2D(-681 + (roadBuilder.getInventoryCount() * 22.7), 0, 681, 69));
-                    if (RoadBuilder.getInventoryCount() < 5) {
-                        roadBuilderView.setTranslateX(300);
-                    } else {
-                        roadBuilderView.setTranslateX(300 - ((roadBuilder.getInventoryCount() * 22.7) - 90));
+                while (animationRoadbuilder == playerObject.getPlasticInv().size()*10) {
+                    if (animationRoadbuilder % 13 == 0) {
+                        System.out.println("handler");
+                        roadView.setViewport(new Rectangle2D(-681 + (roadBuilder.getInventoryCount() * 22.7), 0, 681, 69));
+                        if (RoadBuilder.getInventoryCount() < 5) {
+                            roadBuilderView.setTranslateX(300);
+                        } else {
+                            roadBuilderView.setTranslateX(300 - ((roadBuilder.getInventoryCount() * 22.7) - 90));
+                        }
                     }
+                    animationRoadbuilder++;
                 }
-                animationRoadbuilder++;
+                stopAnimation = true;
             }
 
         }
