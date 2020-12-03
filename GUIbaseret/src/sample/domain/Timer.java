40188cde93383f996e5feba319b6cfe1bff90208
JanikSunke <janik.sunke@hotmail.dk>
2020-12-03 17:38:53 +0100

    reader

--- GUIbaseret/src/sample/domain/Timer.java
+++ GUIbaseret/src/sample/domain/Timer.java
@@ -11,10 +11,10 @@ import java.util.Scanner;
 public class Timer {
     private Date startTime;
     private Date endTime;
-    private double time, time1, time2, time3;
-    private String name;
+    private double time = 30, time1, time2, time3;
+    private String name = "janik";
     private String name1, name2, name3;
-    final private String highScore = "src/sample/highScore.txt";
+    final private File highScore = new File("src/sample/highScore.txt");
 
     public void setStartTime() {
         startTime = new Date();
@@ -25,25 +25,29 @@ public class Timer {
     }
 
     public void timeScore() {
-        long timeScore = endTime.getTime()-startTime.getTime();
+        long timeScore = endTime.getTime() - startTime.getTime();
         long seconds = timeScore / 1000;
         double secondsDisplay = seconds % 60;
         double minutesDisplay = seconds / 60;
-        time = minutesDisplay + secondsDisplay/100;
-        System.out.println("Time: " + (int)minutesDisplay + " minutes and " + (int)secondsDisplay + " seconds");
+        time = minutesDisplay + secondsDisplay / 100;
+        System.out.println("Time: " + (int) minutesDisplay + " minutes and " + (int) secondsDisplay + " seconds");
     }
 
     public void readHighScore() {
         //Henter highscore listen
         Scanner reader;
-        reader = new Scanner(highScore);
-        time1 = reader.nextDouble();
-        name1 = reader.next();
-        time2 = reader.nextDouble();
-        name2 = reader.next();
-        time3 = reader.nextDouble();
-        name3 = reader.next();
-        reader.close();
+        try {
+            reader = new Scanner(highScore);
+            time1 = reader.nextDouble();
+            name1 = reader.next();
+            time2 = reader.nextDouble();
+            name2 = reader.next();
+            time3 = reader.nextDouble();
+            name3 = reader.next();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public String[] setHighScore() {
@@ -58,9 +62,9 @@ public class Timer {
         try {
             PrintWriter writer = new PrintWriter(highScore);
             //ændrer '.' til ',' da reader ikke kan læse '.'
-            writer.print(printTime1.replace(".",",") + " ");
-            writer.print(printTime2.replace(".",",") + " ");
-            writer.print(printTime3.replace(".",",") + " ");
+            writer.print(printTime1.replace(".", ",") + " ");
+            writer.print(printTime2.replace(".", ",") + " ");
+            writer.print(printTime3.replace(".", ",") + " ");
             writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
