45ef6493ecf1987ce0270eb1235f7b49ab559fd5
ValdemarBirk <71699913+ValdemarBirk@users.noreply.github.com>
2020-12-03 14:10:32 +0100

    Ændring af volumen + tilføjelse af samtale

--- GUIbaseret/src/sample/presentation/Controller.java
+++ GUIbaseret/src/sample/presentation/Controller.java
@@ -64,6 +64,8 @@ public class Controller {
     private AudioMusicPlayer backgroundMusic = new AudioMusicPlayer("src/sample/presentation/audio/BackgroundMusic.wav");
     private AudioMusicPlayer roadbuilderCrashSound = new AudioMusicPlayer("src/sample/presentation/audio/RoadbuildCrash.wav");
     private AudioMusicPlayer roadbuilderMovingSound = new AudioMusicPlayer("src/sample/presentation/audio/RoadbuilderMovingSound.wav");
+    private AudioMusicPlayer npcTalk = new AudioMusicPlayer("src/sample/presentation/audio/npcTalking.wav");
+
 
 
     @FXML
@@ -645,6 +647,7 @@ public class Controller {
         playerText.setText("");
         dialogBox.setTranslateY(3000);
         talking = false;
+        npcTalk.AudioStop();
     }
 
     public void showDialogBox() {
@@ -658,6 +661,7 @@ public class Controller {
         playerText.setFont(Font.font("Dialog", FontWeight.BOLD, 11));
         if (Main.game.getCurrentRoom() instanceof Farm) {
             if (spaceCount == 0 && !farmerTalk) {
+                npcTalk.AudioPlayer();
                 talking = true;
                 talkNPC(NPCTextLine, "farmer", 0);
                 talkNPC(NPCTextLine1, "farmer", 1);
@@ -694,6 +698,7 @@ public class Controller {
             }
         } else if (Main.game.getCurrentRoom() instanceof Sdu) {
             if (spaceCount == 0 && !professorTalk) {
+                npcTalk.AudioPlayer();
                 talking = true;
                 talkNPC(NPCTextLine, "professor", 0);
                 talkNPC(NPCTextLine1, "professor", 1);
@@ -714,6 +719,7 @@ public class Controller {
         } else if (Main.game.getCurrentRoom() instanceof Town) {
             if (roadBuilder.getDamaged() > 0) {
                 if (spaceCount == 0 && !mechanicTalk) {
+                    npcTalk.AudioPlayer();
                     talkNPC(NPCTextLine, "mechanic", 0);
                     talkNPC(NPCTextLine1, "mechanic", 1);
                     spaceCount++;
