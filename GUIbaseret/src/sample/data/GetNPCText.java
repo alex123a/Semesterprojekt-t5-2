e473179e00ca1ea09d1d2f9c4ee1d1eef3b2da88
Frederik Nielsen <frederik@icsharp.io>
2020-12-04 13:40:41 +0100

    small changes +  added player text file

--- GUIbaseret/src/sample/data/GetNPCText.java
+++ GUIbaseret/src/sample/data/GetNPCText.java
@@ -6,10 +6,10 @@ import java.nio.file.Paths;
 
 public class GetNPCText {
 
-    public String ReadFile(String path, int num) {
+    public String ReadFile(String path, int index) {
         String line = "";
         try {
-            return Files.readAllLines(Paths.get(path)).get(num);
+            return Files.readAllLines(Paths.get(path)).get(index);
         } catch (IOException e) {
             e.printStackTrace();
         }
