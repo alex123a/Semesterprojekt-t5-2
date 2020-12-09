a8c16b48527ca0dce14c3ad0a6b682127b5abac2
Frederik Nielsen <frederik@icsharp.io>
2020-12-09 12:53:09 +0100

    Added comments to the Toolset class

--- GUIbaseret/src/sample/domain/NPCs/Toolset.java
+++ GUIbaseret/src/sample/domain/NPCs/Toolset.java
@@ -3,11 +3,11 @@ package sample.domain.NPCs;
 import sample.presentation.Main;
 
 public class Toolset {
-    // repair is used for
+    // is used when repairing Roadbuilder
     final private int repair = 25;
     final private String image = "src/sample/presentation/pictures/npc/Tool.png";
 
-    //
+    // removes the damage on Roadbuilder by 25
     public void repairMachine() {
         Main.game.getRoadBuilder().setDamaged(Main.game.getRoadBuilder().getDamaged() - repair);
     }
